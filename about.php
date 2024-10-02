cc5da8e14f7bf34487efbf739d92b602b3fa0390
unknown <your@email.example>
2024-10-02 13:56:23 +0300

    first commit

---
+++ about.php
@@ -0,0 +1,10 @@
+<?php
+
+echo "<br><br>=======POST START========<br><br>";
+var_dump($_POST);
+echo "<br><br>=======POST END REQUEST START========<br><br>";
+var_dump($_REQUEST);
+echo "<br><br>=======REQUEST END SERVER START========<br><br>";
+var_dump($_SERVER);
+echo "<br><br>=======SERVER END========<br><br>";
+
